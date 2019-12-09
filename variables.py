f39b3ba41a0022ec3b2ade79b576e64c52603d70
rameshgowru <ramesh.gowru@gmail.com>
2019-12-09 03:45:03 +0000

    variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3.7
+my_str = "This is a simple string"
+abc = 5 
+
+my_list = [1, 2, 3, 4, 5]
+thistupe = ("x", "y", "z")
+
+
+print(my_str) 
+print("this is integer", abc)
+
+print("this is list", my_list)
+print("this is list 1", my_list[1:]) 
+
+for x in my_list:
+ print(" this is for list:", x , "\n")
+
+
+
+for x in thistupe:
+  print("this is tuple: ", x)
+
+
+print(thistupe[1:])
